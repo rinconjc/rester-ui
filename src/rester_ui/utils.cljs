5c13b8fb3e73a07eb6911c7d639ab9a4a7943ef4
Julio Rincon <jcrinconc@gmail.com>
2020-03-31 09:08:34 +1100

    Refactor

--- src/rester_ui/utils.cljs
+++ src/rester_ui/utils.cljs
@@ -1,5 +1,6 @@
 (ns rester-ui.utils
-  (:require [oops.core :refer [oset! oget ocall]]))
+  (:require [oops.core :refer [oset! oget ocall]]
+            [reagent.core :as r]))
 
 (defn log [& xs]
   (apply println xs))
@@ -15,3 +16,12 @@
     (ocall e "preventDefault")
     (ocall e "stopPropagation")
     false))
+
+(defn with-init [elem mount-fn]
+  (r/create-class
+   {:display-name "with-init"
+    :reagent-render
+    (fn [elem _] elem)
+    :component-did-mount
+    (fn [this]
+      (mount-fn (r/dom-node this)))}))
